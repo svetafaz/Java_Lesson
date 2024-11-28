c1bea0d81cde21ece149dfcd604f3c06b5a35536
svetafaz <svetafaz01@gmail.com>
2024-11-28 13:51:07 +0300

    не могу понять ошибки свои

---
+++ ShopOOP/src/AddStorage.java
@@ -0,0 +1,16 @@
+import model.Product;
+
+import java.util.Arrays;
+
+public class AddStorage {
+
+    public void addStorage(String [] args ){
+        Product[] productsOne = new Product [[3]++;];
+        for (int i = 0; i<=3;i++) {
+            new productsOne [] = Arrays.copyOf(productsOne,productsOne++);
+            System.out.println("На складе:");
+        }
+        }
+
+    }
+
